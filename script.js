55c3856927c7da7856cff7d0538c761fc42010d0
JDoy5519 <jdoyle0106@gmail.com>
2025-05-29 15:10:30 +0100

    put in place book and library constructers

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
+//Skeleton working here
+
 const myLibrary = [];
 
-function Book(name, author, pages, read) {
+function Book(name, author, pages, read, id) {
     this.name = name;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    id = self.crypto.randomUUID();
     this.info = function() {
-        return this.name + " by " + this.author + ", " + this.pages + ", " + this.read
+        return [this.name, this.author, this.pages, this.read, id]
     }
 }
 
-function addBookToLibrary(params) {
-    
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet")
+const Holes = new Book("Holes", "Louis Sachar", "233 pages", "read");
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet");
-
-console.log(theHobbit.info())
+console.log(theHobbit.info());
+console.log(Holes.info());
+console.log(addBookToLibrary(theHobbit.info()));
+console.log(addBookToLibrary(Holes.info()));
+console.log(myLibrary);
