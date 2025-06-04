2596dfc4fd319bb677d91ff5b87e4aa51d005240
JDoy5519 <jdoyle0106@gmail.com>
2025-06-04 17:17:50 +0100

    made modal & filled with form inputs

--- script.js
+++ script.js
@@ -70,3 +70,30 @@ function addData(arr) {
 //example book mapped out onto the table
 addData(theHobbit.info());
 addData(Holes.info());
+
+//toggle the modal
+
+//initialise modal
+
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+
+open.addEventListener ('click', () => {
+    modal_container.classList.add('show');
+}
+);
+
+close.addEventListener ('click', () => {
+    modal_container.classList.remove('show');
+}
+);
+
+//form submission
+
+let username;
+
+document.getElementById("mySubmit").onclick = function () {
+    username = document.getElementById("myText").value;
+    console.log(username);
+}
