f099793c40c87c9e5e29ede2a01cd78ecb5d1214
JDoy5519 <jdoyle0106@gmail.com>
2025-06-05 16:58:34 +0100

    edit the way books are displayed

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //initialiser for the Library array
 let myLibrary = [];
 
+//prevent default behaviour of submit button
+const form = document.getElementById("myForm");
+const submit = document.getElementById("mySubmit");
+
+
 //constructor to create an array from some arguments
 function Book(name, author, pages, read) {
     this.name = name; //assigns the name object to the first argument taken for a new Book
@@ -13,9 +18,16 @@ function Book(name, author, pages, read) {
     }
 }
 
+
+
+
+
 //example arrrays
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", 3)
 const Holes = new Book("Holes", "Louis Sachar", "233", 1);
+
+
+
 
 //function to push a new book array into the library array
 function addBookToLibrary(book) {
@@ -23,174 +35,18 @@ function addBookToLibrary(book) {
     console.log(book.id);
 }
 
+
+
+
 //example arrays being added to the library
 addBookToLibrary(theHobbit);
 addBookToLibrary(Holes);
 
-//variables being declared
 
-//future
-const futureBookName = document.getElementById("future-book-name");
-const futureAuthor = document.getElementById("future-author");
-const futurePages = document.getElementById("future-pages");
-const futureRead = document.getElementById("future-read");
-const futureId = document.getElementById("future-id");
-
-//current
-const currentBookName = document.getElementById("current-book-name");
-const currentAuthor = document.getElementById("current-author");
-const currentPages = document.getElementById("current-pages");
-const currentRead = document.getElementById("current-read");
-const currentId = document.getElementById("current-id");
-
-//past
-const pastBookName = document.getElementById("past-book-name");
-const pastAuthor = document.getElementById("past-author");
-const pastPages = document.getElementById("past-pages");
-const pastRead = document.getElementById("past-read");
-const pastId = document.getElementById("past-id");
-let array = [];
-let readVariable;
-let bookClass;
-const removeButton = document.createElement('button');
-
-//function to append the data of the book to the 'table'
-Object.setPrototypeOf(addData, Book);
+//function to set the prototypes of each function to acess objects
 Object.setPrototypeOf(addBookToLibrary, Book);
-function addData(arr) {
-    array = arr.info();
-    readVariable = arr.read;
-    bookClass = arr.id;
-    if (readVariable === 1) {
-        for (let i = 0; i < array.length; i++) {
-            if (i === 0) {
-                let newName = document.createElement('div');
-                newName.innerHTML = `${array[i]}`;
-                newName.setAttribute = ('class', bookClass);
-                console.log(`${array[i]}`)
-                futureBookName.appendChild(newName);
-            } else if (i === 1) {
-                let newAuthor = document.createElement('div');
-                newAuthor.innerHTML = `${array[i]}`;
-                newAuthor.setAttribute = ('class', bookClass);
-                futureAuthor.appendChild(newAuthor);
-                console.log(array[i])
-            } else if (i === 2) {
-                let newPages = document.createElement('div');
-                newPages.innerHTML = `${array[i]}`;
-                newPages.setAttribute = ('class', bookClass);
-                futurePages.appendChild(newPages);
-                console.log(array[i])
-            } else if (i === 3) {
-                let newRead = document.createElement('div');
-                newRead.innerHTML = `${array[i]}`;
-                newRead.setAttribute = ('class', bookClass);
-                futureRead.appendChild(newRead);
-                console.log(array[i])
-            } else if (i === 4) {
-                let newId = document.createElement('div');
-                newId.innerHTML = `${array[i]}`;
-                removeButton.innerHTML = "Remove";
-                removeButton.setAttribute = ('id', bookClass);
-                newId.setAttribute = ('class', bookClass);
-                futureId.appendChild(newId);
-                newId.appendChild(removeButton);
-                console.log(array[i])
-            }
-            
-        }
-        
-    } else if (readVariable === 2) {
-        
-        for (let i = 0; i < array.length; i++) {
-            if (i === 0) {
-                let newName = document.createElement('div');
-                newName.innerHTML = `${array[i]}`;
-                newName.setAttribute = ('class', bookClass);
-                console.log(`${array[i]}`)
-                currentBookName.appendChild(newName);
-            } else if (i === 1) {
-                let newAuthor = document.createElement('div');
-                newAuthor.innerHTML = `${array[i]}`;
-                newAuthor.setAttribute = ('class', bookClass);
-                currentAuthor.appendChild(newAuthor);
-                console.log(array[i])
-            } else if (i === 2) {
-                let newPages = document.createElement('div');
-                newPages.innerHTML = `${array[i]}`;
-                newPages.setAttribute = ('class', bookClass);
-                currentPages.appendChild(newPages);
-                console.log(array[i])
-            } else if (i === 3) {
-                let newRead = document.createElement('div');
-                newRead.innerHTML = `${array[i]}`;
-                newRead.setAttribute = ('class', bookClass);
-                currentRead.appendChild(newRead);
-                console.log(array[i])
-            } else if (i === 4) {
-                let newId = document.createElement('div');
-                removeButton.innerHTML = "Remove";
-                removeButton.setAttribute = ('id', bookClass);
-                newId.innerHTML = `${array[i]}`;
-                newId.setAttribute = ('class', bookClass);
-                currentId.appendChild(newId);
-                currentId.appendChild(removeButton);
-                console.log(array[i])
-            }
-            
-        }
-
-
-    } else if (readVariable === 3) {
-
-        for (let i = 0; i < array.length; i++) {
-                    if (i === 0) {
-                        let newName = document.createElement('div');
-                        newName.innerHTML = `${array[i]}`;
-                        newName.setAttribute = ('class', bookClass);
-                        console.log(`${array[i]}`)
-                        pastBookName.appendChild(newName);
-                    } else if (i === 1) {
-                        let newAuthor = document.createElement('div');
-                        newAuthor.innerHTML = `${array[i]}`;
-                        newAuthor.setAttribute = ('class', bookClass);
-                        pastAuthor.appendChild(newAuthor);
-                        console.log(array[i])
-                    } else if (i === 2) {
-                        let newPages = document.createElement('div');
-                        newPages.innerHTML = `${array[i]}`;
-                        newPages.setAttribute = ('class', bookClass);
-                        pastPages.appendChild(newPages);
-                        console.log(array[i])
-                    } else if (i === 3) {
-                        let newRead = document.createElement('div');
-                        newRead.innerHTML = `${array[i]}`;
-                        newRead.setAttribute = ('class', bookClass);
-                        pastRead.appendChild(newRead);
-                        console.log(array[i])
-                    } else if (i === 4) {
-                        let newId = document.createElement('div');
-                        newId.innerHTML = `${array[i]}`;
-                        newId.setAttribute = ('class', bookClass);
-                        removeButton.innerHTML = "Remove";
-                        removeButton.setAttribute = ('id', bookClass);
-                        pastId.appendChild(newId);
-                        pastId.appendChild(removeButton);
-                        console.log(array[i])
-                    }
-                    
-                }
-            }
-
-    }    
-
-
-let addData1 = Object.getPrototypeOf(addData);
-console.log(addData1);
-
-//example book mapped out onto the table
-addData(theHobbit);
-addData(Holes);
+Object.setPrototypeOf(createCard, Book);
+Object.setPrototypeOf(submitUserInput, Book);
 
 //toggle the modal
 
@@ -210,9 +66,7 @@ close.addEventListener ('click', () => {
 }
 );
 
-//prevent default behaviour of submit button
-const form = document.getElementById("myForm");
-const submit = document.getElementById("mySubmit");
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -235,9 +89,9 @@ function declareReadValue() {
             }
         }
 
-//onclick function for submit button
 
-submit.onclick = function () {
+//function for submit button
+function submitUserInput() {
     let title = document.getElementById("book-name-input").value;
     console.log(title);
     let titleAuthor = document.getElementById("author-input").value;
@@ -247,15 +101,46 @@ submit.onclick = function () {
     let titleRead = declareReadValue();
     console.log(titleRead);
     let newBook = new Book(title,titleAuthor,titlePages,titleRead);
-    console.log(newBook)
-    addData(newBook.info());
+    console.log(newBook);
+    addBookToLibrary(newBook);
+    createCard(newBook);
+    console.log(myLibrary);
 }
 
-//function to remove all elements of the book
-function removeBookDetails(className) {
-    let elements = document.getElementsByClassName(className);
-    while (elements.length > 0) {
-        elements[0].parentNode.removeChild(elements[0]);
+//onclick function for submit button
+
+//THIS IS FINE
+
+//function to create card
+const futureReads = document.getElementById("future-reads");
+const currentlyReading = document.getElementById("currently-reading");
+const readingHistory = document.getElementById("reading-history");
+const newDiv = document.createElement('div');
+
+function createCard(book) {
+    if (book.read === "I want to read this one!") {
+        newDiv.innerHTML += `<div>Book name: ${book.name}</div>
+        <div>Author: ${book.author}</div>
+        <div>Pages: ${book.pages}</div>
+        <div>ID: ${book.id}</div>
+        <div>${book.read}</div>`;
+        futureReads.appendChild(newDiv);
+    } else if (book.read === "I am reading this one!") {
+        newDiv.innerHTML += `<div>Book name: ${book.name}</div>
+        <div>Author: ${book.author}</div>
+        <div>Pages: ${book.pages}</div>
+        <div>ID: ${book.id}</div>
+        <div>${book.read}</div>`;
+        currentlyReading.appendChild(newDiv);
+    } else if (book.read === "I have read this one!") {
+        newDiv.innerHTML += `<div>Book name: ${book.name}</div>
+        <div>Author: ${book.author}</div>
+        <div>Pages: ${book.pages}</div>
+        <div>ID: ${book.id}</div>
+        <div>${book.read}</div>`;
+        readingHistory.appendChild(newDiv);
     }
-}
+};
 
+
+console.log(myLibrary);
