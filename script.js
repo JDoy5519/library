948fd47f42e33b91fda0043541a76566f8f448c7
JDoy5519 <jdoyle0106@gmail.com>
2025-06-05 14:43:53 +0100

    able to receive input and assign to page

--- script.js
+++ script.js
@@ -14,12 +14,13 @@ function Book(name, author, pages, read) {
 }
 
 //example arrrays
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet")
-const Holes = new Book("Holes", "Louis Sachar", "233 pages", "read");
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", 3)
+const Holes = new Book("Holes", "Louis Sachar", "233", 1);
 
 //function to push a new book array into the library array
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    console.log(book.id);
 }
 
 //example arrays being added to the library
@@ -27,49 +28,169 @@ addBookToLibrary(theHobbit);
 addBookToLibrary(Holes);
 
 //variables being declared
-const bookName = document.getElementById("book-name");
-const author = document.getElementById("author");
-const pages = document.getElementById("pages");
-const read = document.getElementById("read");
-const id = document.getElementById("id");
+
+//future
+const futureBookName = document.getElementById("future-book-name");
+const futureAuthor = document.getElementById("future-author");
+const futurePages = document.getElementById("future-pages");
+const futureRead = document.getElementById("future-read");
+const futureId = document.getElementById("future-id");
+
+//current
+const currentBookName = document.getElementById("current-book-name");
+const currentAuthor = document.getElementById("current-author");
+const currentPages = document.getElementById("current-pages");
+const currentRead = document.getElementById("current-read");
+const currentId = document.getElementById("current-id");
+
+//past
+const pastBookName = document.getElementById("past-book-name");
+const pastAuthor = document.getElementById("past-author");
+const pastPages = document.getElementById("past-pages");
+const pastRead = document.getElementById("past-read");
+const pastId = document.getElementById("past-id");
+let array = [];
+let readVariable;
+let bookClass;
+const removeButton = document.createElement('button');
 
 //function to append the data of the book to the 'table'
+Object.setPrototypeOf(addData, Book);
+Object.setPrototypeOf(addBookToLibrary, Book);
 function addData(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (i === 0) {
-            let newName = document.createElement('div');
-            newName.innerHTML = `${arr[i]}`;
-            bookName.appendChild(newName);
-            console.log(arr[i])
-        } else if (i === 1) {
-            let newAuthor = document.createElement('div');
-            newAuthor.innerHTML = `${arr[i]}`;
-            author.appendChild(newAuthor);
-            console.log(arr[i])
-        } else if (i === 2) {
-            let newPages = document.createElement('div');
-            newPages.innerHTML = `${arr[i]}`;
-            pages.appendChild(newPages);
-            console.log(arr[i])
-        } else if (i === 3) {
-            let newRead = document.createElement('div');
-            newRead.innerHTML = `${arr[i]}`;
-            read.appendChild(newRead);
-            console.log(arr[i])
-        } else if (i === 4) {
-            let newId = document.createElement('div');
-            newId.innerHTML = `${arr[i]}`;
-            id.appendChild(newId);
-            console.log(arr[i])
-        }
+    array = arr.info();
+    readVariable = arr.read;
+    bookClass = arr.id;
+    if (readVariable === 1) {
+        for (let i = 0; i < array.length; i++) {
+            if (i === 0) {
+                let newName = document.createElement('div');
+                newName.innerHTML = `${array[i]}`;
+                newName.setAttribute = ('class', bookClass);
+                console.log(`${array[i]}`)
+                futureBookName.appendChild(newName);
+            } else if (i === 1) {
+                let newAuthor = document.createElement('div');
+                newAuthor.innerHTML = `${array[i]}`;
+                newAuthor.setAttribute = ('class', bookClass);
+                futureAuthor.appendChild(newAuthor);
+                console.log(array[i])
+            } else if (i === 2) {
+                let newPages = document.createElement('div');
+                newPages.innerHTML = `${array[i]}`;
+                newPages.setAttribute = ('class', bookClass);
+                futurePages.appendChild(newPages);
+                console.log(array[i])
+            } else if (i === 3) {
+                let newRead = document.createElement('div');
+                newRead.innerHTML = `${array[i]}`;
+                newRead.setAttribute = ('class', bookClass);
+                futureRead.appendChild(newRead);
+                console.log(array[i])
+            } else if (i === 4) {
+                let newId = document.createElement('div');
+                newId.innerHTML = `${array[i]}`;
+                removeButton.innerHTML = "Remove";
+                removeButton.setAttribute = ('id', bookClass);
+                newId.setAttribute = ('class', bookClass);
+                futureId.appendChild(newId);
+                newId.appendChild(removeButton);
+                console.log(array[i])
+            }
             
-    }
-    
-}
+        }
+        
+    } else if (readVariable === 2) {
+        
+        for (let i = 0; i < array.length; i++) {
+            if (i === 0) {
+                let newName = document.createElement('div');
+                newName.innerHTML = `${array[i]}`;
+                newName.setAttribute = ('class', bookClass);
+                console.log(`${array[i]}`)
+                currentBookName.appendChild(newName);
+            } else if (i === 1) {
+                let newAuthor = document.createElement('div');
+                newAuthor.innerHTML = `${array[i]}`;
+                newAuthor.setAttribute = ('class', bookClass);
+                currentAuthor.appendChild(newAuthor);
+                console.log(array[i])
+            } else if (i === 2) {
+                let newPages = document.createElement('div');
+                newPages.innerHTML = `${array[i]}`;
+                newPages.setAttribute = ('class', bookClass);
+                currentPages.appendChild(newPages);
+                console.log(array[i])
+            } else if (i === 3) {
+                let newRead = document.createElement('div');
+                newRead.innerHTML = `${array[i]}`;
+                newRead.setAttribute = ('class', bookClass);
+                currentRead.appendChild(newRead);
+                console.log(array[i])
+            } else if (i === 4) {
+                let newId = document.createElement('div');
+                removeButton.innerHTML = "Remove";
+                removeButton.setAttribute = ('id', bookClass);
+                newId.innerHTML = `${array[i]}`;
+                newId.setAttribute = ('class', bookClass);
+                currentId.appendChild(newId);
+                currentId.appendChild(removeButton);
+                console.log(array[i])
+            }
+            
+        }
+
+
+    } else if (readVariable === 3) {
+
+        for (let i = 0; i < array.length; i++) {
+                    if (i === 0) {
+                        let newName = document.createElement('div');
+                        newName.innerHTML = `${array[i]}`;
+                        newName.setAttribute = ('class', bookClass);
+                        console.log(`${array[i]}`)
+                        pastBookName.appendChild(newName);
+                    } else if (i === 1) {
+                        let newAuthor = document.createElement('div');
+                        newAuthor.innerHTML = `${array[i]}`;
+                        newAuthor.setAttribute = ('class', bookClass);
+                        pastAuthor.appendChild(newAuthor);
+                        console.log(array[i])
+                    } else if (i === 2) {
+                        let newPages = document.createElement('div');
+                        newPages.innerHTML = `${array[i]}`;
+                        newPages.setAttribute = ('class', bookClass);
+                        pastPages.appendChild(newPages);
+                        console.log(array[i])
+                    } else if (i === 3) {
+                        let newRead = document.createElement('div');
+                        newRead.innerHTML = `${array[i]}`;
+                        newRead.setAttribute = ('class', bookClass);
+                        pastRead.appendChild(newRead);
+                        console.log(array[i])
+                    } else if (i === 4) {
+                        let newId = document.createElement('div');
+                        newId.innerHTML = `${array[i]}`;
+                        newId.setAttribute = ('class', bookClass);
+                        removeButton.innerHTML = "Remove";
+                        removeButton.setAttribute = ('id', bookClass);
+                        pastId.appendChild(newId);
+                        pastId.appendChild(removeButton);
+                        console.log(array[i])
+                    }
+                    
+                }
+            }
+
+    }    
+
+
+let addData1 = Object.getPrototypeOf(addData);
+console.log(addData1);
 
 //example book mapped out onto the table
-addData(theHobbit.info());
-addData(Holes.info());
+addData(theHobbit);
+addData(Holes);
 
 //toggle the modal
 
@@ -89,6 +210,14 @@ close.addEventListener ('click', () => {
 }
 );
 
+//prevent default behaviour of submit button
+const form = document.getElementById("myForm");
+const submit = document.getElementById("mySubmit");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
 //initialise form variables
 
 let title;
@@ -98,22 +227,35 @@ let titleRead;
 
 //function to select correct radio option
 
-function displayRadioValue() {
+function declareReadValue() {
             let ele = document.getElementsByName('read-status');
-            for (i = 0; i < ele.length; i++) {
-                if (ele[i].checked)
-                titleRead = ele[i].value;
-                console.log(titleRead);
+            for (j = 0; j < ele.length; j++) {
+                if (ele[j].checked)
+                    return titleRead = ele[j].value;
             }
         }
 
 //onclick function for submit button
 
-document.getElementById("mySubmit").onclick = function () {
+submit.onclick = function () {
     let title = document.getElementById("book-name-input").value;
     console.log(title);
     let titleAuthor = document.getElementById("author-input").value;
+    console.log(titleAuthor);
     let titlePages = document.getElementById("pages-input").value;
-    let titleRead = displayRadioValue();
+    console.log(titlePages);
+    let titleRead = declareReadValue();
     console.log(titleRead);
+    let newBook = new Book(title,titleAuthor,titlePages,titleRead);
+    console.log(newBook)
+    addData(newBook.info());
 }
+
+//function to remove all elements of the book
+function removeBookDetails(className) {
+    let elements = document.getElementsByClassName(className);
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
