cbd8f185bfd0c39c635502de2c249acc885f6079
JDoy5519 <jdoyle0106@gmail.com>
2025-06-06 14:51:30 +0100

    fix java to give extra functiuons to the cards

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //initialiser for the Library array
 let myLibrary = [];
 
+
 //prevent default behaviour of submit button
 const form = document.getElementById("myForm");
 const submit = document.getElementById("mySubmit");
@@ -19,14 +20,9 @@ function Book(name, author, pages, read) {
 }
 
 
-
-
-
 //example arrrays
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", 3)
-const Holes = new Book("Holes", "Louis Sachar", "233", 1);
-
-
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "I want to read this one!")
+const Holes = new Book("Holes", "Louis Sachar", "233", "I am reading this one!");
 
 
 //function to push a new book array into the library array
@@ -38,17 +34,12 @@ function addBookToLibrary(book) {
 
 
 
-//example arrays being added to the library
-addBookToLibrary(theHobbit);
-addBookToLibrary(Holes);
-
 
 //function to set the prototypes of each function to acess objects
 Object.setPrototypeOf(addBookToLibrary, Book);
 Object.setPrototypeOf(createCard, Book);
 Object.setPrototypeOf(submitUserInput, Book);
-
-//toggle the modal
+Object.setPrototypeOf(removeFromLibrary, Book);
 
 //initialise modal
 
@@ -65,7 +56,6 @@ close.addEventListener ('click', () => {
     modal_container.classList.remove('show');
 }
 );
-
 
 
 form.addEventListener("submit", (e) => {
@@ -107,40 +97,137 @@ function submitUserInput() {
     console.log(myLibrary);
 }
 
-//onclick function for submit button
-
 //THIS IS FINE
 
-//function to create card
+//variables for function to create card
 const futureReads = document.getElementById("future-reads");
 const currentlyReading = document.getElementById("currently-reading");
 const readingHistory = document.getElementById("reading-history");
-const newDiv = document.createElement('div');
+const removeThis = document.getElementsByClassName('remove');
+
+
 
 function createCard(book) {
+const newDiv = document.createElement('div');
+const removeButton = document.createElement('button');
     if (book.read === "I want to read this one!") {
         newDiv.innerHTML += `<div>Book name: ${book.name}</div>
         <div>Author: ${book.author}</div>
         <div>Pages: ${book.pages}</div>
         <div>ID: ${book.id}</div>
         <div>${book.read}</div>`;
+        newDiv.setAttribute('id', book.id);
         futureReads.appendChild(newDiv);
+        removeButton.innerHTML += 'Remove this book!'
+        removeButton.setAttribute('class',"remove");
+        removeButton.onclick = function () {
+            deleteOnClick(this);
+            console.log(myLibrary)
+        };
+        newDiv.appendChild(removeButton);
+        const statusButton = document.createElement('button');
+        statusButton.innerHTML = 'Change Read Status';
+        statusButton.onclick = function () {
+            cycleReadStatus(book);       
+            removeDiv(this);             
+            createCard(book);            
+                };
+        newDiv.appendChild(statusButton);
     } else if (book.read === "I am reading this one!") {
         newDiv.innerHTML += `<div>Book name: ${book.name}</div>
         <div>Author: ${book.author}</div>
         <div>Pages: ${book.pages}</div>
         <div>ID: ${book.id}</div>
         <div>${book.read}</div>`;
+        newDiv.setAttribute('id', book.id);
         currentlyReading.appendChild(newDiv);
+        removeButton.innerHTML += 'Remove this book!'
+        removeButton.setAttribute('class',"remove");
+        removeButton.onclick = function () {
+            deleteOnClick(this);
+            console.log(myLibrary)
+        };
+        newDiv.appendChild(removeButton);
+        const statusButton = document.createElement('button');
+        statusButton.innerHTML = 'Change Read Status';
+        statusButton.onclick = function () {
+            cycleReadStatus(book);       
+            removeDiv(this);             
+            createCard(book);            
+                };
+        newDiv.appendChild(statusButton);
     } else if (book.read === "I have read this one!") {
         newDiv.innerHTML += `<div>Book name: ${book.name}</div>
         <div>Author: ${book.author}</div>
         <div>Pages: ${book.pages}</div>
         <div>ID: ${book.id}</div>
         <div>${book.read}</div>`;
+        newDiv.setAttribute('id', book.id);
         readingHistory.appendChild(newDiv);
+        removeButton.innerHTML += 'Remove this book!'
+        removeButton.setAttribute('class',"remove");
+        removeButton.onclick = function () {
+            deleteOnClick(this);
+            
+        };
+        newDiv.appendChild(removeButton);
+        const statusButton = document.createElement('button');
+        statusButton.innerHTML = 'Change Read Status';
+        statusButton.onclick = function () {
+            cycleReadStatus(book);       
+            removeDiv(this);             
+            createCard(book);            
+                };
+        newDiv.appendChild(statusButton);
     }
 };
 
+//function to delete div from the DOM
+let deleteId;
+
+function getParentId(el) {
+    let deleteId = el.parentNode.id;
+    return deleteId;
+}
+
+function removeDiv(el) {
+    let removeId = getParentId(el);
+    let elementToRemove = document.getElementById(removeId);
+    if (elementToRemove) elementToRemove.remove();
+}
+
+//function to delete the object from the library
+function removeFromLibrary(arr, value) {
+    const filtered = arr.filter(obj => obj.id !== value);
+    return myLibrary = filtered;
+};
+
+//put all of these together to create one function for the onlick
+
+function deleteOnClick(el) {
+    removeDiv(el);
+    let parentId = getParentId(el);
+    removeFromLibrary(myLibrary, parentId);
+}
+
+//function to alter read status
+
+function cycleReadStatus(book) {
+    const statuses = [
+        "I want to read this one!",
+        "I am reading this one!",
+        "I have read this one!"
+    ];
+    const currentIndex = statuses.indexOf(book.read);
+    const nextIndex = (currentIndex + 1) % statuses.length;
+    book.read = statuses[nextIndex];
+}
 
 console.log(myLibrary);
+
+//example arrays being added to the library
+addBookToLibrary(theHobbit);
+addBookToLibrary(Holes);
+
+createCard(theHobbit);
+createCard(Holes);
